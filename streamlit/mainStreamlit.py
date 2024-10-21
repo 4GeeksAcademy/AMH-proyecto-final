98c1e85e2f8fce74deb8d39e84683245090ab9a7
postigogo <hpostigogomez@gmail.com>
2024-10-21 21:00:57 +0200

    Commit cambio directorio styles.css 21-10.2024

--- streamlit/mainStreamlit.py
+++ streamlit/mainStreamlit.py
@@ -1,9 +1,13 @@
 import streamlit as st
 from funciones import cargar_css
 from streamlit_extras.switch_page_button import switch_page
+import os
 
-# Aplicamos el CSS personalizado y el fondo
-cargar_css('style/styles.css')
+#Directorio actual
+base_dir = os.path.dirname(__file__)
+
+# Cargar DataFrames
+cargar_css(os.path.join(base_dir, '..','streamlit', 'style', 'styles.css'))
 
 def main():
     if "page" not in st.session_state:
